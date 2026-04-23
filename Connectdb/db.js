4741a615e7e917e7059eafe756ddf2561f8f173c
Joseph-cavele <josephcavele@gmail.com>
2026-04-23 15:20:30 +0200

    feat: add MongoDB connection and server setup

--- Connectdb/db.js
+++ Connectdb/db.js
@@ -6,11 +6,11 @@ dotenv.config()
 
 const uri =process.env.MONGO_URI
  export const CONNECT_DB=async()=>{
-    
+    console.log(uri)
 
     try {
         const conn=await mongoose.connect(uri)
-        console.log('Database is connected')
+        
         
     } catch (error) {
         console.error(`Error:${error}`)
